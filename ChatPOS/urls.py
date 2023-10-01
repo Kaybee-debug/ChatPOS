cb23c0c477a239e1d3562b75aa78425be91183e9
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2023-10-01 03:30:58 +0200

    modified files

--- ChatPOS/urls.py
+++ ChatPOS/urls.py
@@ -20,4 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('Home.urls')),
+    path("business/",include('Business.urls')),
+    path('Accounts/', include("Accounts.urls") )
+    
 ]
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now=True)
+   
